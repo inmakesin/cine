2e997f2840b4ccd92b005ffd8331ef9d7b79ec39
Mohammed Shibil V <shibilshibi009@gmail.com>
2023-09-28 01:56:44 +0530

    added

---
+++ cinema_proj/cine_app/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+app_name = 'cineapp'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('movie/<int:movie_id>/',views.detail, name='detail'),
+    path('add/',views.add, name='add'),
+    path('update/<int:id>/',views.update, name='update'),
+    path('delete/<int:id>/',views.delete, name='delete')
+]
